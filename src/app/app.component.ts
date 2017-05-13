23488b7d439e4ae7b388b79b87fd41e6a9010f53
Georgy PC <georgekagan@gmail.com>
2017-05-13 16:04:12 +0300

    ngfor footer links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,34 @@ import { AppState } from './app.service';
 export class AppComponent implements OnInit {
   public appLogo = 'assets/img/eaziup-logo.png';
   public slogan = 'Turning Ideas Into Results…';
+  public footerNav = [
+    {label: 'Company', items: [
+      {label: 'About', link: '/stub'},
+      {label: 'How it works?', link: '/stub'},
+      {label: 'Careers', link: '/stub'},
+      {label: 'Our team', link: '/stub'},
+      {label: 'Press', link: '/stub'},
+      {label: 'Confidentiality', link: '/stub'},
+      {label: 'Terms & conditions', link: '/stub'},
+    ]},
+    {label: 'Contact', items: [
+      {label: 'Contact us', link: '/stub'},
+      {label: 'Support', link: '/stub'},
+      {label: 'Supplier team', link: '/stub'},
+      {label: 'Project / Sellers team', link: '/stub'}
+    ]},
+    {label: 'Suppliers', items: [
+      {label: 'Sign up', link: '/stub'},
+      {label: 'How it works', link: '/stub'},
+      {label: 'Supplier database', link: '/stub'},
+      {label: 'Team database', link: '/stub'}
+    ]},
+    {label: 'Projects / Sellers', items: [
+      {label: 'Add a project now', link: '/stub'},
+      {label: 'How it works', link: '/stub'},
+      {label: 'Project database', link: '/stub'}
+    ]}
+  ];
 
   constructor(
     public appState: AppState
