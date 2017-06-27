7f05960fd0f0cd7733fc03f002cad1bf2147e082
Georgy PC <georgekagan@gmail.com>
2017-06-27 22:41:50 +0300

    shit forgot to add this to the last commit

---
+++ src/app/components/project/project-form-buyer-info.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {slideDown} from '../../consts/animations';
+
+@Component({
+  selector: 'project-form-buyer-info',
+  templateUrl: './project-form-buyer-info.component.html',
+  animations: [slideDown]
+})
+export class ProjectFormBuyerInfoComponent {
+  @Input() myForm: FormGroup;
+  @Input() formErrors: object = {};
+  @Input() gotError: Function;
+  @Input() countries: string[] = [];
+}
---
+++ src/app/components/project/project-form-project-info.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {slideDown} from '../../consts/animations';
+
+@Component({
+  selector: 'project-form-project-info',
+  templateUrl: './project-form-project-info.component.html',
+  animations: [slideDown]
+})
+export class ProjectFormProjectInfoComponent {
+  @Input() myForm: FormGroup;
+  @Input() formErrors: object = {};
+  @Input() gotError: Function;
+}
