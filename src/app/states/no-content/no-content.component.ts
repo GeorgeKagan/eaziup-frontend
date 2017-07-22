c8bb1a790c3d6e615df89aae0abc0e2fa78cbaa6
Georgy PC <georgekagan@gmail.com>
2017-07-22 17:15:33 +0300

    restyle 404

--- src/app/states/no-content/no-content.component.ts
+++ src/app/states/no-content/no-content.component.ts
@@ -3,9 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'no-content',
   template: `
-    <div class="container pb-5">
-      <div class="col-sm-12">
-        <h1 class="display-4 text-danger">
+    <div class="container pt-5 pb-5">
+      <div class="ml-auto mr-auto mt-5 text-center" style="width: 400px">
+        <h1 class="display-4 pt-5 text-danger">
           404: not found!
         </h1>
         <p class="lead text-muted">Sorry about that...</p>
