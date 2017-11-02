7845a116a2e0afdbe3ff71252e1ab73c5b0ae20a
Georgy PC <georgekagan@gmail.com>
2017-11-02 23:02:25 +0200

    finish exporting icons to own file

--- src/app/states/how-it-works/how-it-works.component.ts
+++ src/app/states/how-it-works/how-it-works.component.ts
@@ -2,13 +2,17 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import {ICONS} from '../../consts/icons';
 
 @Component({
   selector: 'how-it-works',
   templateUrl: 'how-it-works.component.html'
 })
 export class HowItWorksComponent implements OnInit {
+  ICONS: object;
+  
   constructor() {
+    this.ICONS = ICONS;
   }
 
   public ngOnInit() {
