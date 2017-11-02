4098a346741ef0a0ac54a51b3baa05fe22362732
Georgy PC <georgekagan@gmail.com>
2017-11-02 23:51:30 +0200

    view project state

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
 import {Restangular} from "ngx-restangular";
+import {CONFIG} from '../consts/config';
+
+const DAYS_IN_WEEK = 7;
 
 @Injectable()
 export class ProjectService {
-
   constructor(private rest: Restangular) {
   }
 
@@ -25,5 +27,35 @@ export class ProjectService {
 
   public removeProject(projectId: number) {
     return this.rest.one('project', projectId).remove();
+  }
+
+  /**
+   * Calculate delivery date of a project by adding milestones' duration to the start date.
+   * @param {string} startDate
+   * @param {string} milestones
+   * @returns {string}  Format: YYYY-MM-DD
+   */
+  public static calculateDeliveryDate(startDate: string, milestones: Array<any>): string {
+    let
+      date = new Date(Date.parse(startDate)),
+      durationWeeks = milestones.map(x => x.timespan).reduce((x, y) => x + y);
+
+    date.setDate(date.getDate() + durationWeeks * DAYS_IN_WEEK);
+
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
+
+  /**
+   * Calculate compenstation for completion of a project by finding the bracket the project dev/milestone counts fall into.
+   * @param {number} devCount
+   * @param {number} milestoneCount
+   * @returns {number}
+   */
+  public static calculateCompenstation(devCount: number, milestoneCount: number): number {
+    let
+      brackets = CONFIG.WIZARD.PRICING.filter(x => x.devs === devCount && milestoneCount <= x.maxStones),
+      minBracket = brackets.sort((a, b) => a.maxStones - b.maxStones)[0];
+
+    return minBracket.ourPrice;
   }
 }
--- src/app/states/view-project/view-project.component.ts
+++ src/app/states/view-project/view-project.component.ts
@@ -1,21 +1,49 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
+import {DatePipe, DecimalPipe} from '@angular/common';
+import {ICONS} from '../../consts/icons';
+import {ProjectService} from '../../services/project.service';
 
 @Component({
   selector: 'view-project',
   templateUrl: 'view-project.component.html'
 })
 export class ViewProjectComponent implements OnInit {
-  private project: object = null;
+  public blocks: object[] = [];
+  public sidebarItems: object[] = [];
+  private project: any = null;
 
   constructor(private route: ActivatedRoute,
-              private auth: AuthService) {
+              private datePipe: DatePipe,
+              private decimalPipe: DecimalPipe,
+              public auth: AuthService) {
   }
 
   public ngOnInit() {
     this.route.data.subscribe(data => {
       this.project = data.project || null;
+      this.project.milestones = JSON.parse(this.project.milestones_json);
+
+      this.blocks = [
+        {icon: ICONS.QUICK, title: 'Quick intro', body: this.project.desc},
+        {icon: ICONS.REQS, title: 'Dev Requirements', list: [
+          {title: 'Developers', body: `Team of <strong>${this.project.dev_count}</strong>`},
+          {title: 'Technology stack', body: this.project.tech_reqs},
+          {title: 'Communication', body: this.project.dev_reqs},
+          {title: 'Operating systems', body: this.project.os_reqs},
+        ]},
+        {icon: ICONS.INFO, title: 'Full description', body: this.project.full_desc}
+      ];
+
+      this.sidebarItems = [
+        {title: 'Starts', body: this.datePipe.transform(this.project.start_date)},
+        {title: 'Delivery', body: this.datePipe.transform(ProjectService.calculateDeliveryDate(this.project.start_date, this.project.milestones))},
+        {title: 'Status', body: this.project.status.toUpperCase()},
+        {title: 'Created', body: this.datePipe.transform(this.project.created_at)},
+        {title: 'Updated', body: this.datePipe.transform(this.project.updated_at)},
+        {title: 'Compensation', body: this.decimalPipe.transform(ProjectService.calculateCompenstation(this.project.dev_count, this.project.milestones.length)) + ' ILS'}
+      ];
     });
   }
 
