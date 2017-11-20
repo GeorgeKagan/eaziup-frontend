e686a5bc64622b886f2421e452093c658577af89
George Kagan <georgekagan@gmail.com>
2017-11-20 23:40:47 +0200

    fix bootstrap 4 beta 2 breaking change

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Restangular} from "ngx-restangular";
+import {Restangular} from 'ngx-restangular';
 
 @Injectable()
 export class ApplicationService {
